99b353950b824f4c8fb0bfbc3096970cfff8292f
maazihad <maazihadin@gmail.com>
2023-05-23 02:54:54 +0600

    Search implement added by Toy Name.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // =================================================================Create Express 
@@ -29,20 +29,20 @@ const client = new MongoClient(uri, {
    }
 });
 // =====================================================================Verify Jot
-const verifyJWT = (req, res, next) => {
-   const authorization = req.headers.authorization;
-   if (!authorization) {
-      return res.status(401).send({ error: true, message: "unauthorized access" });
-   }
-   const token = authorization.split(' ')[1];
-   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-      if (error) {
-         return res.status(401).send({ error: true, message: "unauthorized access." });
-      }
-      req.decoded = decoded;
-      next();
-   });
-};
+// const verifyJWT = (req, res, next) => {
+//    const authorization = req.headers.authorization;
+//    if (!authorization) {
+//       return res.status(401).send({ error: true, message: "unauthorized access" });
+//    }
+//    const token = authorization.split(' ')[1];
+//    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+//       if (error) {
+//          return res.status(401).send({ error: true, message: "unauthorized access." });
+//       }
+//       req.decoded = decoded;
+//       next();
+//    });
+// };
 
 async function run() {
    try {
@@ -54,37 +54,35 @@ async function run() {
       const addAToyCollections = client.db('toy_racer').collection('myToys');
       const allToys = client.db('toy_racer').collection('allToys');
       // =============================================================Post :JWT
-      app.post('/jwt', (req, res) => {
-         const user = req.body;
-         const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' });
-         res.send({ token });
-      });
+      // app.post('/jwt', (req, res) => {
+      //    const user = req.body;
+      //    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' });
+      //    res.send({ token });
+      // });
 
+
+      //==============================================
+      const indexKeys = { toyName: 1 };
+      const indexOptions = { name: "toyName" };
+      const result = await allToys.createIndex(indexKeys, indexOptions);
+      // console.log(result);
+      // console.log(
+      //    "Pinged your deployment. You successfully connected to MongoDB!"
+      // );
       //============================================================get :gallery route
       app.get('/galleries', async (req, res) => {
          const result = await galleryCollections.find({}).toArray();
          res.send(result);
       });
 
-      // ===========================================================GET : My toys route
-      app.get('/myToys', verifyJWT, async (req, res) => {
-         const decoded = req.decoded;
-         if (decoded.email !== req.query.email) {
-            return res.status(403).send({ error: 1, message: "Access denied" });
-         }
-         let query = {};
-         if (req.query?.email) {
-            query = { email: req.query.email };
-         };
-         const result = await addAToyCollections.find(query).toArray();
-         res.send(result);
-         // console.log(result);
-      });
 
+      // ==========================================================GET : all toys route
       app.get('/allToys', async (req, res) => {
          const result = await allToys.find({}).toArray();
          res.send(result);
       });
+
+      // ============================================================Get : single toys route
       app.get('/allToys/:id', async (req, res) => {
          const id = req.params.id;
          const query = { _id: new ObjectId(id) };
@@ -102,16 +100,47 @@ async function run() {
          };
          const toys = await allToys.findOne(query, options);
          res.send(toys);
+         console.log(toys);
       });
+
+
+
+      // ==========================================================Search Implement
+      app.get("/searchByToyName/:text", async (req, res) => {
+         const searchText = req.params.text;
+         const result = await allToys.find({
+            $or: [
+               { toyName: { $regex: searchText, $options: "i" } }
+            ],
+         }).toArray();
+         res.send(result);
+      });
+
+
+      // ===========================================================GET : My toys route
+      // app.get('/myToys', verifyJWT, async (req, res) => {
+      //    const decoded = req.decoded;
+      //    if (decoded.email !== req.query.email) {
+      //       return res.status(403).send({ error: 1, message: "Access denied" });
+      //    }
+      //    let query = {};
+      //    if (req.query?.email) {
+      //       query = { email: req.query.email };
+      //    };
+      //    const result = await addAToyCollections.find(query).toArray();
+      //    res.send(result);
+      //    // console.log(result);
+      // });
+
+
+      // another way =============================================get : my toys route
+
 
 
       // ===========================================================Post: My Toys route
       app.post('/myToys', async (req, res) => {
          const addAToy = req.body;
          const result = await addAToyCollections.insertOne(addAToy);
-         if (!addAToy) {
-            return res.status(404).send({ error: true, message: "Body not found." });
-         }
          res.send(result);
          // console.log(result);
       });
